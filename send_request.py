03e1e9ef703f74264b68036cd7bf0af33c708bae
wangqingcui <120371332+wangqingcui@users.noreply.github.com>
2024-05-28 12:37:43 +0800

    Update send_request.py

--- send_request.py
+++ send_request.py
@@ -1,13 +1,21 @@
 import requests
 
-url = 'https://api.vvhan.com/api/text/joke?type=json'  # 替换为你要请求的 URL
+url = 'https://api.tjise.edu.cn/usercenter/connect/token'  # 替换为你要请求的 URL
+data = {
+    "username": "15142424683",
+    "password": "www15963",
+    "grant_type": "password",
+    "code_verifier": "",
+    "client_id": "DDE1F5ACAF194674B13167269861FB7D"
+}
 
-# 发送 GET 请求
-response = requests.get(url)
-print(response )
+# 发送 POST 请求
+response = requests.post(url, data)
+print(response)
+
 if response.status_code == 200:
     print('请求成功！')
-    data = response.json()  # 解析响应数据
-    print(data)
+    response_data = response.json()  # 解析响应数据
+    print(response_data)
 else:
     print('请求失败，状态码：', response.status_code)
